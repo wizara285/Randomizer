8be0b69eb6d3a4ca8fe446a35a0fe5387b78903f
bartosha285 <bartosha285@gmail.com>
2023-09-06 17:19:02 +0300

    created Randomizer

---
+++ Randomizer/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Randomizer.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GenerateRandomNumber(int minValue, int maxValue)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(minValue, maxValue + 1);
+
+            ViewBag.RandomNumber = randomNumber;
+
+            return View("Index");
+        }
+    }
+}
